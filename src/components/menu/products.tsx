6e442ac6c8667c113de890efe48dab7e856bac01
kushjindal14 <kushs.18.beis@acharya.ac.in>
2022-11-22 12:24:59 +0530

    filtering products checkbox

--- src/components/menu/products.tsx
+++ src/components/menu/products.tsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import List from "./list-view";
 import Grid from "./grid-view";
@@ -13,7 +13,6 @@ const Product = () => {
   useEffect(() => {
     getdata();
   }, []);
-
 
   const getdata = async () => {
     try {
@@ -41,48 +40,46 @@ const Product = () => {
   if (!product.length) return <div>Loading...</div>;
   return (
     <div>
-    <div>
-      <div className="top">
-        <h3>List</h3>
-        <label className="switch">
-          <input type="checkbox" onClick={() => setActive(!active)} />
-          <span className="slider"></span>
-        </label>
-        <h3>Grid</h3>
-      </div>
-      <hr />
-      <h3 className="total-products">Found total {product.length} items</h3>
-      <hr />
+      <div>
+        <div className="top">
+          <h3>List</h3>
+          <label className="switch">
+            <input type="checkbox" onClick={() => setActive(!active)} />
+            <span className="slider"></span>
+          </label>
+          <h3>Grid</h3>
+        </div>
+        <hr />
+        <h3 className="total-products">Found total {product.length} items</h3>
+        <hr />
         {filteredResults.map((filteredResult) => (
-            <div className="products">
-               <div className="filtering-checkbox">
-                <input type="checkbox"/>
-                <label>{filteredResult.category}</label>
-              </div>
-          <div>
-            <div className="categories">
-              {filteredResult.category} ({filteredResult.items.length})
+          <div className="products">
+            <div className="filtering-checkbox">
+              <input type="checkbox" />
+              <label>{filteredResult.category} ({filteredResult.items.length})</label>
             </div>
-            {filteredResult.items.map((item: IProduct) => (
-              <div>
-                {active ? (
-                  <div className="lists-items">
-                  <List item={item} /> 
-                  </div>
-                ) : (
-                  <div className="grid-items">
-                    <Grid item={item} />
-                  </div>
-                )}
+            <div>
+              <div className="categories">
+                {filteredResult.category} ({filteredResult.items.length})
               </div>
-            ))}
-          </div>
+              {filteredResult.items.map((item: IProduct) => (
+                <div>
+                  {active ? (
+                    <div className="lists-items">
+                      <List item={item} />
+                    </div>
+                  ) : (
+                    <div className="grid-items">
+                      <Grid item={item} />
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
-      </div>
-    // </div>
-    //  </div>
+    </div>
   );
 };
 
