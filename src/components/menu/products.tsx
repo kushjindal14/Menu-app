e743e2c8feb77fb4c940a472365b8e153bf9da07
kushjindal14 <kushs.18.beis@acharya.ac.in>
2022-11-22 12:05:44 +0530

    filtering-checkbox inside map

--- src/components/menu/products.tsx
+++ src/components/menu/products.tsx
@@ -53,30 +53,12 @@ const Product = () => {
       <hr />
       <h3 className="total-products">Found total {product.length} items</h3>
       <hr />
-      <div className="products">
-       <div className="checkbox-filter">
-          <form>
-            <h3>Filter Products</h3>
-            <div>
-                <input type="checkbox"/>
-                <label>Men's Clothing</label>
-            </div>
-             <div>
-                <input type="checkbox"/>
-                <label>Jewelery</label>
-            </div>
-             <div>
-                <input type="checkbox"/>
-                <label>Electronics</label>
-            </div>
-             <div>
-                <input type="checkbox"/>
-                <label>Women's Clothing</label>
-            </div>
-        </form>  
-        </div>
-      <div>
         {filteredResults.map((filteredResult) => (
+            <div className="products">
+               <div className="filtering-checkbox">
+                <input type="checkbox"/>
+                <label>{filteredResult.category}</label>
+              </div>
           <div>
             <div className="categories">
               {filteredResult.category} ({filteredResult.items.length})
@@ -95,11 +77,12 @@ const Product = () => {
               </div>
             ))}
           </div>
+          </div>
         ))}
       </div>
       </div>
-    </div>
-     </div>
+    // </div>
+    //  </div>
   );
 };
 
